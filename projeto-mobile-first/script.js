f030ea674758f23a1358e7b217b9906e198fbee5
Caio0-0 <126476942+Caio0-0@users.noreply.github.com>
2023-04-18 14:50:03 -0300

    novo

---
+++ projeto-mobile-first/script.js
@@ -0,0 +1,14 @@
+function clicM(){
+    if (its.style.display == 'block'){
+        its.style.display = 'none'
+    }else{
+        its.style.display = 'block'
+    }
+}
+function mudouT(){
+    if (window.innerWidth >= 768){
+        its.style.display = 'block'
+    }else{
+        its.style.display = 'none'
+    }
+}
